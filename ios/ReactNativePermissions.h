fcd722f77771f153059db23fb1290ba4067a60f2
Sushant Sardeshpande <ssardeshpande@elivotech.com>
2018-08-02 15:36:39 +0530

    Update ReactNativePermissions.h

--- ios/ReactNativePermissions.h
+++ ios/ReactNativePermissions.h
@@ -6,13 +6,7 @@
 //  Copyright © 2016 Yonah Forst. All rights reserved.
 //
 
-#if __has_include(<React/RCTBridgeModule.h>)
-  #import <React/RCTBridgeModule.h>
-#elif __has_include("React/RCTBridgeModule.h")
-  #import "React/RCTBridgeModule.h"
-#else
-  #import "RCTBridgeModule.h"
-#endif
+#import <React/RCTBridgeModule.h>
 
 @interface ReactNativePermissions : NSObject <RCTBridgeModule>
 
